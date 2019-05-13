37f22102d6c693ef8b2c372563e2d08bf22c58f4
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2019-05-13 09:45:44 +0530

    Added Payment Gateway

--- category.php
+++ category.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	require('connect.php');
 	$name = $_POST['category'];
@@ -22,29 +21,4 @@
 		echo mysqli_error($conn);
 	}
 
-=======
-<?php
-	require('connect.php');
-	$name = $_POST['category'];
-	$conn = mysqli_connect($servername,$username,$password,$database);
-	if($conn)
-	{
-		$query = "INSERT into categories(`name`) VALUES('".$name."')";
-		$result = mysqli_query($conn,$query);
-		if($result)
-		{
-			echo "INSERTED";
-			header("location:adminpanel.php");
-		}
-		else
-		{
-			echo "Error  - ".mysqli_error($conn);
-		}
-	}
-	else
-	{
-		echo mysqli_error($conn);
-	}
-
->>>>>>> Update 2.0
 ?>
--- subcategory.php
+++ subcategory.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	require('connect.php');
 	$name = $_POST['category'] ;
@@ -23,30 +22,4 @@
 		echo mysqli_error($conn);
 	}
 
-=======
-<?php
-	require('connect.php');
-	$name = $_POST['category'] ;
-	$main = $_POST['main_category'] ;
-	$conn = mysqli_connect($servername,$username,$password,$database);
-	if($conn)
-	{
-		$query = "INSERT into subcategories(`name`,`maincategory`) VALUES('".$name."',$main)";
-		$result = mysqli_query($conn,$query);
-		if($result)
-		{
-			echo "INSERTED";
-			header("location:adminpanel.php");
-		}
-		else
-		{
-			echo "Error  - ".mysqli_error($conn);
-		}
-	}
-	else
-	{
-		echo mysqli_error($conn);
-	}
-
->>>>>>> Update 2.0
 ?>
