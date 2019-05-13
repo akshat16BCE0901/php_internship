fb062eea1dcbbd4936f651ca104991a2e2a7153f
Akshat Singhal <akshat.singhal2016@vitstudent.ac.in>
2019-05-13 10:07:57 +0530

    Final Commit

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+		<link rel="shortcut icon" href="assets/images/favicon.jpg">
 		<script src="assets/js/wow.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
